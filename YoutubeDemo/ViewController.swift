2bb75e27f8b8aff0b4c36f016c9839b985cab07f
Lei, Xinye <xlei33@gatech.edu>
2016-03-10 20:12:07 -0500

    Initial Commit

---
+++ YoutubeDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YoutubeDemo
+//
+//  Created by xinye lei on 16/3/10.
+//  Copyright © 2016年 xinye lei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
